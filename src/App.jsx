2a0e34fc50fb0d6e5bd64f8e4476712a3b6b520d
Saket Kandari <saket12kandari@gmail.com>
2025-11-04 22:22:36 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>7OAKS</div>
+      <p>Treks and Tours</p>
+    </>
+  );
+}
+
+export default App;
