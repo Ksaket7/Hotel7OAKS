81eef69d0e2f8bed1bd7a5e0c947af394cff5835
Saket Kandari <saket12kandari@gmail.com>
2025-11-09 18:35:55 +0530

    added hotel component and removed tours component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,7 +2,7 @@ import Hero from "../components/Hero";
 import Stats from "../components/Stats";
 import Packages from "../components/Packages";
 import Treks from "../components/Treks";
-import Tours from "../components/Tours";
+import Tours from "../components/Hotels";
 import WhyUs from "../components/WhyUs";
 import Testimonials from "../components/Testimonials";
 import ImageMarquee from "../components/ImageMarquee";
